6e5ca971dca49e1e07e8947b674e7febbfaaf16d
saitovixtiyor <ixtyorsaitov@gmail.com>
2026-09-16 15:18:48 +0500

    headert title component

---
+++ src/components/shared/HeaderTitle.tsx
@@ -0,0 +1,37 @@
+import { Box, Button, Title } from "@mantine/core";
+import React from "react";
+import { Link } from "wouter";
+interface Props {
+  title: string;
+  buttonText?: string;
+  children?: React.ReactNode;
+  href?: string;
+}
+
+export default function HeaderTitle({
+  title,
+  buttonText,
+  href,
+  children,
+}: Props) {
+  return (
+    <Box
+      w={"100%"}
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        marginTop: "10px",
+      }}
+    >
+      <Title>{title}</Title>
+      {children ??
+        (href ? (
+          <Link href={href}>
+            <Button>{buttonText}</Button>
+          </Link>
+        ) : (
+          <Button>{buttonText}</Button>
+        ))}
+    </Box>
+  );
+}
